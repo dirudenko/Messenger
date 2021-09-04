efd681a17c57ecfd0b4e5ac6d6cc7673a345ff84
Dmitry Rudenko <dirudenko@icloud.com>
2021-09-04 11:24:31 +0300

    starting project

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Dmitry on 04.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+print("hello")
+    
+  }
+
+
+}
+
